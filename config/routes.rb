876db317c626831cd112a7900353ebef0a2f3c99
RezaMrtz <reza.mrtz@gmail.com>
2020-02-09 00:29:40 +0330

    Just some warm up with rails

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  root 'home#index'
+  get 'about', to: 'home#about'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
