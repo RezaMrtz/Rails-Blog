9e9d413c16cf9130ca883acbd68c322ec77cffd7
RezaMrtz <reza.mrtz@gmail.com>
2020-02-13 01:53:18 +0330

    Validation for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3, maximum: 12 }
+  validates :body, presence: true, length: { minimum: 15, maximum: 30 }
 end
