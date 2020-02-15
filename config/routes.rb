a4d539cb45b5b3d280cbccdb8a498b3531f174b6
RezaMrtz <reza.mrtz@gmail.com>
2020-02-16 00:27:41 +0330

    Add Home controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  
+  get '/', to: 'home#home'
+  
+  get '/about', to: 'home#about'
+  
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
