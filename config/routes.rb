29ac8869fc38dd2138aaaede50e0e446af7fedb2
RezaMrtz <reza.mrtz@gmail.com>
2020-02-18 02:31:39 +0330

    Add and styling to navigation and New Article

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  
-  get '/', to: 'home#home'
-  
+  root to: 'home#home'
   get '/about', to: 'home#about'
-  
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
