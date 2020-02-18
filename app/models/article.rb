4a88e08b58fb27317a5927b5ce153658fd063dc4
RezaMrtz <reza.mrtz@gmail.com>
2020-02-18 23:51:14 +0330

    Style list view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximum: 25 }
-  validates :body, presence: true, length: { minimum: 15, maximum: 50 }
+  validates :body, presence: true, length: { minimum: 15, maximum: 150 }
 end
