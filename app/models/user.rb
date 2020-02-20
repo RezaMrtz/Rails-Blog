d4267bcd1258b970009006981a612cd2ff8229da
RezaMrtz <reza.mrtz@gmail.com>
2020-02-20 23:01:20 +0330

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
     has_many :comments
+    validates :username,
+               presence: true,
+               length: {minimum:3, maximum:25},
+               uniqueness: {case_sensitive: false}
+
+    validates :email,
+              format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
