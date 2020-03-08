de5a9d980cab0003c6eb00c1e5267eedf4a51ced
RezaMrtz <reza.mrtz@gmail.com>
2020-03-08 21:43:33 +0330

    Fix some bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # Resources
+  root to: 'home#home'
   resources :comments
   resources :users
-  root to: 'home#home'
+  resources :articles
+  # Navigation
   get '/about', to: 'home#about'
   get '/contact', to: 'home#contact'
   get '/signup', to: 'users#new'
-  resources :articles
   # Sessions
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
